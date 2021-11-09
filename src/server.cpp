c8de61856f8c6cb28cc41e239f5e8a930453b8f7
Marwan Ashraf <maroashraf428@gmail.com>
2021-11-09 23:02:56 +0200

    Initial commit

---
+++ src/server.cpp
@@ -0,0 +1,15 @@
+//
+// Created by the-quartz on 09/11/2021.
+//
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Hello World\n";
+    return 0;
+}
+
+
